dc1ff40208694cc36ad6165f1ec18f960f4054bb
Vanibels <vredgarden@gmail.com>
2026-04-06 19:59:40 +0200

    feat add Alias class

---
+++ src/core/Alias.h
@@ -0,0 +1,30 @@
+#include <filesystem>
+#include <string>
+
+namespace fs = std::filesystem;
+
+typedef enum alias {open, launch} t_alias;
+const char* aliass[] = {"open","launch"};
+
+class Alias {
+    private:
+        t_alias type;
+        std::string name;
+        fs::path path;
+        fs::path ConfigFile;
+    public:
+        Alias(t_alias t,
+             std::string n, 
+             fs::path p, 
+             fs::path cf ) : 
+             type(t), 
+             name(n), 
+             path(p), 
+             ConfigFile(cf) {}
+        void setPath(fs::path p);
+        void getByName(std::string n);
+        void save();
+        void unSave();
+        fs::path getPath() const;
+        std::string get() const;
+};
