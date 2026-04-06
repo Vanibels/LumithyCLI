e39e9daf33e1ce2e25cbd9fda3f9a6644cd6d6da
Vanibels <vredgarden@gmail.com>
2026-04-06 18:53:15 +0200

    feat : pass std::map read has a global fontion

--- src/lumithy.cpp
+++ src/lumithy.cpp
@@ -174,28 +174,6 @@ void showHelp(){
     saveLogs(cmd, logs::info);
 }
 
-std::map<std::string, std::string> read(std::string section, std::string file){
-    std::ifstream checkFile(file);
-    if (!checkFile.good()) {
-        std::ofstream createFile(file);
-        createFile << "[open]" << std::endl;
-        createFile << "[launch]" << std::endl;
-        createFile.close();
-    }
-    const char* iniPath = file.c_str();
-    std::map<std::string, std::string> openAliases;
-    char buffer[2048]; 
-    GetPrivateProfileStringA(section.c_str(), NULL, "", buffer, 2048, iniPath);
-    char* key = buffer;
-    while (*key != '\0') {
-        char value[MAX_PATH];
-        GetPrivateProfileStringA(section.c_str(), key, "NOT_FOUND", value, MAX_PATH, iniPath);
-        openAliases[key] = value;
-        key += strlen(key) + 1;
-    }
-    return openAliases;
-}
-
 std::string read(std::string section,std::string key, std::string file){
     std::ifstream checkFile(file);
     if (!checkFile.good()) {
