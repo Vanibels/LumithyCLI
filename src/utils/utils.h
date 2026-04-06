927d7d330c76b9cd2f45c56be8969d288ebc920f
Vanibels <vredgarden@gmail.com>
2026-04-06 18:53:15 +0200

    feat : pass std::map read has a global fontion

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -13,3 +13,25 @@ namespace color{
     inline const std::string red = "\033[31m";
     inline const std::string yellow = "\033[33m";
 }
+
+std::map<std::string, std::string> read(std::string section, std::string file){
+    std::ifstream checkFile(file);
+    if (!checkFile.good()) {
+        std::ofstream createFile(file);
+        createFile << "[open]" << std::endl;
+        createFile << "[launch]" << std::endl;
+        createFile.close();
+    }
+    const char* iniPath = file.c_str();
+    std::map<std::string, std::string> openAliases;
+    char buffer[2048]; 
+    GetPrivateProfileStringA(section.c_str(), NULL, "", buffer, 2048, iniPath);
+    char* key = buffer;
+    while (*key != '\0') {
+        char value[MAX_PATH];
+        GetPrivateProfileStringA(section.c_str(), key, "NOT_FOUND", value, MAX_PATH, iniPath);
+        openAliases[key] = value;
+        key += strlen(key) + 1;
+    }
+    return openAliases;
+}
